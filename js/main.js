487431b5d1bda6497737e7ea89b6e06d0174b0e8
Muhammad Bilal <bilalmughl.sialkot@gmail.com>
2023-08-18 02:17:40 +0500

    update

--- js/main.js
+++ js/main.js
@@ -1,132 +1,124 @@
 $(document).ready(function() {
-    // Define the element you want to add the class to
+    // Sticky header behavior
     var targetElement = $('.yu-header');
+    var scrollPoint = 100;
 
-    // Define the scroll point at which you want to add the class
-    var scrollPoint = 100; // Adjust this value as needed
-
-    // Bind the scroll event to the window
     $(window).scroll(function() {
-        // Get the current scroll position
         var scrollPosition = $(window).scrollTop();
-
-        // Check if the scroll position is greater than the defined scroll point
         if (scrollPosition > scrollPoint) {
-        // Add the desired class to the target element
-        targetElement.addClass('sticky');
+            targetElement.addClass('sticky');
         } else {
-        // Remove the class if the scroll position is less than the scroll point
-        targetElement.removeClass('sticky');
+            targetElement.removeClass('sticky');
         }
     });
 
-
-
+    // Tab switching functionality
     $(".tabs a").click(function(e) {
         e.preventDefault();
-
-        // Get the target tab content div and image based on the clicked tab
         var targetIndex = $(this).attr("class").replace("tab-nav-", "");
         var targetContent = $(".tab-content-" + targetIndex);
         var targetImage = $(".tab-img-" + targetIndex + " img");
 
-        // Hide all tab content divs and images
-        $(".content > [class*='tab-content-']").hide().removeClass("active"); // Remove "active" class from all content
+        $(".content > [class*='tab-content-']").hide().removeClass("active");
         $(".tab-content-image > [class*='tab-img-']").hide();
 
-        // Remove active class from all tab links
         $(".tabs a").removeClass("active");
-
-        // Add active class to the clicked tab link
         $(this).addClass("active");
 
-        // Show the selected tab content div and image
-        targetContent.show().addClass("active"); // Add "active" class to the selected content
-        targetImage.show().addClass("active"); // Add "active" class to the selected content
+        targetContent.show().addClass("active");
+        targetImage.show().addClass("active");
     });
 
-    // Add active class on click
+    // Handle active state for sidebar navigation
     $('.yu-asidebar-container li a').on('click', function(event) {
-        // event.preventDefault();
         $('.yu-asidebar-container li a').removeClass('active');
         $(this).addClass('active');
     });
 
-
-    // $(".yu-tab-outer-wrapper .yu-tab-content-nav button").click(function(e) {
-    //     e.preventDefault();
-    //     // If the clicked button already has the "active" class, do nothing
-    //         if ($(this).hasClass("active")) {
-    //             return;
-    //         }
-    //     // Get the target tab content div and image based on the clicked tab
-    //     var targetClass = $(this).attr("class");
-    //     var targetIndex = targetClass.replace("tab-nav-", "");
-    //     var targetContent = $(".yu-tab-outer-wrapper .tab-nav-" + targetIndex + "-content");
-    //     var targetImage = $(".yu-tab-outer-wrapper .tab-nav-" + targetIndex + "-content .yu-tab-thumbnail img");
-        
-    //     // Hide all tab content divs and images
-    //     $(".yu-tab-outer-wrapper .yu-tab-content-nav-area > [class*='tab-nav-']").hide();
-        
-    //     // Remove active class from all tab links and tab content divs
-    //     $(".yu-tab-outer-wrapper .yu-tab-content-nav button").removeClass("active");
-    //     $(".yu-tab-outer-wrapper .yu-tab-content-nav-area > [class*='tab-nav-']").removeClass("active");
-        
-    //     // Add active class to the clicked tab link and tab content div
-    //     $(this).addClass("active");
-        
-    //     // Show the selected tab content div and image
-    //     targetContent.show().addClass("active");;
-    //     targetImage.show();
-    // });
-    
-    // $(".yu-tab-outer-wrapper2 .yu-tab-content-nav button").click(function(e) {
-    //     e.preventDefault();
-    //     // If the clicked button already has the "active" class, do nothing
-    //         if ($(this).hasClass("active")) {
-    //             return;
-    //         }
-    //     // Get the target tab content div and image based on the clicked tab
-    //     var targetClass = $(this).attr("class");
-    //     var targetIndex = targetClass.replace("tab-nav-", "");
-    //     var targetContent = $(".yu-tab-outer-wrapper2 .tab-nav-" + targetIndex + "-content");
-    //     var targetImage = $(".yu-tab-outer-wrapper2 .tab-nav-" + targetIndex + "-content .yu-tab-thumbnail img");
-        
-    //     // Hide all tab content divs and images
-    //     $(".yu-tab-outer-wrapper2 .yu-tab-content-nav-area > [class*='tab-nav-']").hide();
-        
-    //     // Remove active class from all tab links and tab content divs
-    //     $(".yu-tab-outer-wrapper2 .yu-tab-content-nav button").removeClass("active");
-    //     $(".yu-tab-outer-wrapper2 .yu-tab-content-nav-area > [class*='tab-nav-']").removeClass("active");
-        
-    //     // Add active class to the clicked tab link and tab content div
-    //     $(this).addClass("active");
-        
-    //     // Show the selected tab content div and image
-    //     targetContent.show().addClass("active");;
-    //     targetImage.show();
-    // });
+    // Tab switching for yu-tab-outer-wrapper
     $('.yu-tab-content-nav button').on('click', function() {
-        if ($(this).hasClass("active")) {
-            return;
-        }
         var targetClass = $(this).attr("class");
         var targetIndex = targetClass.replace("tab-nav-", "");
         var $parentWrapper = $(this).closest('.yu-tab-outer-wrapper');
         var targetContent = $parentWrapper.find(".tab-nav-" + targetIndex + "-content");
 
-        // Remove "active" class from all buttons and content
         $parentWrapper.find('.yu-tab-content-nav button').removeClass('active');
         $parentWrapper.find('.yu-tab-content-nav-area div').removeClass('active');
 
-        // Add "active" class to the clicked button and content
         $(this).addClass('active');
         targetContent.addClass('active');
 
-        // Hide all content of other tabs
         $parentWrapper.find('.yu-tab-content-nav-area [class*="tab-nav-"]').hide();
-
-        // Show the content of the clicked tab
         targetContent.show();
+    });
+
+    // Clone tab content navigation for mobile
+    $('.yu-tab-outer-wrapper').each(function() {
+        var $parentWrapper = $(this);
+        var $mobileNavTab = $parentWrapper.find('.mobile-nav-tab');
+        var $tabContentNavClone = $parentWrapper.find('.yu-tab-content-nav').clone();
+        $tabContentNavClone.appendTo($mobileNavTab);
+
+        $mobileNavTab.find('button').on('click', function() {
+            var targetClass = $(this).attr("class");
+            var targetIndex = targetClass.replace("tab-nav-", "");
+            var targetContent = $parentWrapper.find(".tab-nav-" + targetIndex + "-content");
+
+            $parentWrapper.find('.yu-tab-content-nav button').removeClass('active');
+            $parentWrapper.find('.yu-tab-content-nav-area div').removeClass('active');
+
+            $(this).addClass('active');
+            targetContent.addClass('active');
+
+            $parentWrapper.find('.yu-tab-content-nav-area [class*="tab-nav-"]').hide();
+            targetContent.show();
+        });
+    });
+
+    // Add "has-item" class to parent li for sub-menu
+    $('.sub-menu').each(function() {
+        $(this).closest('li').addClass('has-item');
+    });
+
+    // Clone navigation content for mobile sidebar
+    var navContent = $(".col-nav").html();
+    $(".nav-sidr-wrap").html(navContent);
+
+    // Toggle mobile menu icon and body class
+    $("#menu-icon").click(function() {
+        $(this).toggleClass("menu-open");
+        $("body").toggleClass("is-open");
+    });
+
+    // Handle submenu interaction based on window width
+    function handleSubmenuInteraction() {
+        if ($(window).width() <= 768) {
+            $(".col-nav ul li").click(function () {
+                $(this).find(".sub-menu").slideToggle();
+            });
+        } else {
+            $(".col-nav ul li").hover(
+                function () {
+                    $(this).find(".sub-menu").slideDown();
+                    $(this).addClass("active");
+                },
+                function () {
+                    $(this).find(".sub-menu").slideUp();
+                    $(this).removeClass("active");
+                }
+            );
+        }
+    }
+
+    handleSubmenuInteraction();
+
+    // Adjust submenu behavior on window resize
+    $(window).on("resize", function () {
+        if ($(window).width() <= 768) {
+            $(".col-nav ul li").off("mouseenter mouseleave");
+        } else {
+            $(".col-nav ul li").off("click");
+            handleSubmenuInteraction();
+        }
     });
 });
