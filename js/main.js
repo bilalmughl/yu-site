7774e950b9f53d1ec5659b32767928d757667ad5
Muhammad Bilal <bilalmughl.sialkot@gmail.com>
2023-08-20 14:51:22 +0500

    update

--- js/main.js
+++ js/main.js
@@ -12,7 +12,12 @@ $(document).ready(function() {
         }
     });
 
-    // Tab switching functionality
+    $(".has-item > a").click(function(event) {
+        event.preventDefault();
+        $(".sub-menu").not($(this).next()).slideUp();
+        $(this).next(".sub-menu").slideToggle();
+    });
+
     $(".tabs a").click(function(e) {
         e.preventDefault();
         var targetIndex = $(this).attr("class").replace("tab-nav-", "");
@@ -29,13 +34,11 @@ $(document).ready(function() {
         targetImage.show().addClass("active");
     });
 
-    // Handle active state for sidebar navigation
-    $('.yu-asidebar-container li a').on('click', function(event) {
+    $(".yu-asidebar-container li a").on('click', function(event) {
         $('.yu-asidebar-container li a').removeClass('active');
         $(this).addClass('active');
     });
 
-    // Tab switching for yu-tab-outer-wrapper
     $('.yu-tab-content-nav button').on('click', function() {
         var targetClass = $(this).attr("class");
         var targetIndex = targetClass.replace("tab-nav-", "");
@@ -52,7 +55,6 @@ $(document).ready(function() {
         targetContent.show();
     });
 
-    // Clone tab content navigation for mobile
     $('.yu-tab-outer-wrapper').each(function() {
         var $parentWrapper = $(this);
         var $mobileNavTab = $parentWrapper.find('.mobile-nav-tab');
@@ -75,50 +77,52 @@ $(document).ready(function() {
         });
     });
 
-    // Add "has-item" class to parent li for sub-menu
     $('.sub-menu').each(function() {
         $(this).closest('li').addClass('has-item');
     });
 
-    // Clone navigation content for mobile sidebar
     var navContent = $(".col-nav").html();
     $(".nav-sidr-wrap").html(navContent);
 
-    // Toggle mobile menu icon and body class
     $("#menu-icon").click(function() {
         $(this).toggleClass("menu-open");
         $("body").toggleClass("is-open");
     });
 
-    // Handle submenu interaction based on window width
-    function handleSubmenuInteraction() {
-        if ($(window).width() <= 768) {
-            $(".col-nav ul li").click(function () {
-                $(this).find(".sub-menu").slideToggle();
-            });
-        } else {
-            $(".col-nav ul li").hover(
-                function () {
-                    $(this).find(".sub-menu").slideDown();
-                    $(this).addClass("active");
-                },
-                function () {
-                    $(this).find(".sub-menu").slideUp();
-                    $(this).removeClass("active");
-                }
-            );
-        }
-    }
-
-    handleSubmenuInteraction();
-
-    // Adjust submenu behavior on window resize
-    $(window).on("resize", function () {
+    $(window).on("resize", function() {
         if ($(window).width() <= 768) {
             $(".col-nav ul li").off("mouseenter mouseleave");
         } else {
             $(".col-nav ul li").off("click");
             handleSubmenuInteraction();
         }
+    });
+
+    $('.meeting-indicator').each(function(index) {
+        var indicator = $(this);
+        var h5 = indicator.find('.meeting-title h5');
+        var meetingtitle = indicator.find('.meeting-title');
+        $('.connecting, .joined').hide();
+
+        var dataDelay = parseInt(indicator.data('delay'));
+        var dataShow = parseInt(indicator.data('show'));
+
+        indicator.fadeOut();
+        setTimeout(function() {
+            indicator.fadeIn().removeClass('hide');
+        }, dataShow);
+
+        h5.fadeOut(0);
+
+        setTimeout(function() {
+            h5.fadeIn();
+            indicator.find('.ringing').hide();
+            indicator.find('.connecting').show();
+        }, dataShow * 2);
+
+        setTimeout(function() {
+            indicator.find('.connecting').hide();
+            indicator.find('.joined').show().closest('.meeting-indicator').addClass("meeting-indicator-joind");
+        }, dataDelay * 2);
     });
 });
